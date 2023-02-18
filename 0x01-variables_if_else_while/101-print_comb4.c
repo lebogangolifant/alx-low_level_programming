24b78cd6def6de09c48c673c0093b8e7d7a2837e
lebogang <lebogangodeveloper@gmail.com>
2023-02-18 03:49:29 -0800

    print combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int digit_1, digit_2, digit_3;
 
-	for (digit_1 = 0; digit_1 < 7; digit_1++)
+	for (digit_1 = 0; digit_1 < 8; digit_1++)
 	{
-		for (digit_2 = digit_1 + 1; digit_2 < 8; digit_2++)
+		for (digit_2 = digit_1 + 1; digit_2 < 9; digit_2++)
 		{
-			for (digit_3 = digit_2 + 1; digit_3 < 9; digit_3++)
+			for (digit_3 = digit_2 + 1; digit_3 < 10; digit_3++)
 			{
 				putchar(digit_1 + 48);
 				putchar(digit_2 + 48);
