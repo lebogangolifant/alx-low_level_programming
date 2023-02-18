ab6f5e12b848b51a8f6bd27a3d9647a202ec0f51
lebogang <lebogangodeveloper@gmail.com>
2023-02-18 03:31:50 -0800

    print combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,20 +13,25 @@ int main(void)
 
 	for (digit_1 = 0; digit_1 < 100; digit_1++)
 	{
-		for (digit_2 = digit_1; digit_2 < 100; digit_2++)
+		for (digit_2 = 0; digit_2 < 100; digit_2++)
 		{
-			putchar(digit_1 / 10 + '0');
-			putchar(digit_1 % 10 + '0');
-			putchar(' ');
-			putchar(digit_2 / 10 + '0');
-			putchar(digit_2 % 10 + '0');
-			if (digit_1 != 99 || digit_2 != 99)
+			if (digit_1 <  digit_2)
 			{
-				putchar(',');
+				putchar(digit_1 / 10 + '0');
+				putchar(digit_1 % 10 + '0');
 				putchar(' ');
+				putchar(digit_2 / 10 + '0');
+				putchar(digit_2 % 10 + '0');
+				if (digit_1 != 99 || digit_2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
+
+	putchar('\n');
 
 	return (0);
 }
