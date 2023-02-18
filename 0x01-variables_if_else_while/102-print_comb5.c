d1804fdba13d0d236635c891acd6fa9a52b50c5d
lebogang <lebogangodeveloper@gmail.com>
2023-02-18 03:15:23 -0800

    print combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two two-digit numbers.
+ *
+ * Return: Returns 0
+ *
+ */
+
+int main(void)
+{
+	int digit_1, digit_2;
+
+	for (digit_1 = 0; digit_1 < 100; digit_1++)
+	{
+		for (digit_2 = digit_1; digit_2 < 100; digit_2++)
+		{
+			putchar(digit_1 / 10 + '0');
+			putchar(digit_1 % 10 + '0');
+			putchar(' ');
+			putchar(digit_2 / 10 + '0');
+			putchar(digit_2 % 10 + '0');
+			if (digit_1 != 99 || digit_2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	return (0);
+}
