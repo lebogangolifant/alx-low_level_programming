8df51e061ad3389fc4700db8d2b7609243b4d330
lebogang <lebogangodeveloper@gmail.com>
2023-02-21 22:12:41 -0800

    prints the sum of all the multiples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,3 +5,15 @@
   *						below 1024 (excluded)
   * Return: Nothing.
   */
+
+int main(void)
+{
+	int i, result;
+	int endPoint = 1024;
+
+	for (i = 0; i < endPoint; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+			result += i;
+	printf("%d\n", result);
+	return (0);
+}
