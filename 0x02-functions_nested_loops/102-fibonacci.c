7fef223a0922aa4216254b26564ccd3abf853c48
lebogang <lebogangodeveloper@gmail.com>
2023-02-21 22:17:34 -0800

    prints the first 50 Fibonacci nums

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,3 +5,28 @@
   *
   * Return: Nothing.
   */
+
+int main(void)
+{
+	long int i, j, k, sum;
+
+	j = 1;
+
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		} else
+		{
+			printf("%ld\n", j);
+		}
+		sum = j + k;
+		j = k;
+		k = sum;
+	}
+
+	return (0);
+}
