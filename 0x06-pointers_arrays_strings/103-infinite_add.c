fe6ec8ba8c35a2c53ba18ee83b5aebe305113466
lebogang <lebogangodeveloper@gmail.com>
2023-03-02 08:25:16 -0800

    todo

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,77 +1,78 @@
 #include "main.h"
-#include <stdio.h>
-#include <stddef.h>
-#include <string.h>
 
 /**
- * get_digit - get the digit value of a character
- * @c: the character to convert to a digit value
- *
- * Return: the digit value of the character, or -1 if it's not a digit
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
  */
 
-int get_digit(char c)
+void rev_string(char *n)
 {
-	if (c >= '0' && c <= '9')
-		return (c - '0');
-	return (-1);
-}
+	int i = 0;
+	int j = 0;
+	char temp;
 
-/**
- * reverse_string - reverse a string in place
- * @s: the string to reverse
- */
-
-void reverse_string(char *s)
-{
-	int i, j;
-	char c;
-
-	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+	while (*(n + i) != '\0')
 	{
-		c = s[i];
-		s[i] = s[j];
-		s[j] = c;
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
 	}
 }
 
 /**
- * infinite_add - add two strings representing integers
- * @n1: the first string
- * @n2: the second string
- * @r: the buffer to store the result
- * @size_r: the size of the buffer
- *
- * Return: a pointer to the result string, or NULL if the result does not fit
- *         in the buffer
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
-	int i, j, k = 0;
-	int carry = 0;
-	int digit1, digit2, sum;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	/* Check if the result will fit in the buffer */
-	if (len1 + len2 > size_r)
-		return (NULL);
-
-	/* Add digits from right to left */
-	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 ||
-			carry; i--, j--, k++)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		digit1 = i >= 0 ? get_digit(n1[i]) : 0;
-		digit2 = j >= 0 ? get_digit(n2[j]) : 0;
-		sum = digit1 + digit2 + carry;
-		carry = sum >= 10;
-		r[k] = sum % 10 + '0';
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-
-	r[k] = '\0';
-
-	/* Reverse the result string */
-	reverse_string(r);
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
 	return (r);
 }
