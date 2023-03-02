41f2e92ef177e6cbc712fe676b53a089060b73aa
lebogang <lebogangodeveloper@gmail.com>
2023-03-02 00:56:51 -0800

    update code to a compact solution

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,6 +2,7 @@
 
 /**
  * print_number - prints an integer
+ *
  * @n: integer to be printed
  *
  * Return: void
@@ -9,20 +10,16 @@
 
 void print_number(int n)
 {
-	int divisor = 1;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-		n = -n;
 		_putchar('-');
+		num = -num;
 	}
 
-	while (n / divisor > 9)
-		divisor *= 10;
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	while (divisor != 0)
-	{
-		_putchar((n / divisor) % 10 + '0');
-		divisor /= 10;
-	}
+	_putchar((num % 10) + '0');
 }
