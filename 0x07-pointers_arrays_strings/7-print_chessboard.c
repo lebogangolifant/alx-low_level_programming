63247b1e4d67dcddd7f805e490948d2b92d3f2a0
Lebogang <lebogangodeveloper@gmail.com>
2023-03-07 08:41:31 +0200

    fix syntax

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,9 +14,10 @@ void print_chessboard(char (*a)[8])
 	for (row = 0; row < 8; row++)
 	{
 		for (col = 0; col < 8; col++)
+		{
 			putchar(a[row][col]);
+		}
+		putchar('\n');
 	}
-
-	putchar('\n');
 }
 
