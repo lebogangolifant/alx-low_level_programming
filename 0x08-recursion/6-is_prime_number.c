38dae611e08ab31b048cd6c6c3614985f2f24ee9
Lebogang <lebogangodeveloper@gmail.com>
2023-03-09 11:04:28 +0200

    remove loops

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,24 +1,44 @@
 #include "main.h"
 
 /**
- * is_prime_number - checks if a number is prime
- * @n: input integer
+ * is_divisible - Helper function checks if a number is divisible by a divisor.
+ * @num: The number to be checked.
+ * @div: The divisor.
  *
- * Return: 1 if prime, 0 otherwise
+ * Return: If the number is divisible - 1.
+ *         If the number is not divisible - 0.
+ */
+
+int is_divisible(int num, int div)
+{
+	if (num % div == 0)
+	{
+		return (1);
+	}
+	if (div * div > num)
+	{
+		return (0);
+	}
+	return (is_divisible(num, div + 1));
+}
+
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to be checked.
+ *
+ * Return: If the integer is not prime - 0.
+ *         If the number is prime - 1.
  */
 
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
-		return (0);
-
-	for (i = 2; i <= n / 2; i++)
 	{
-		if (n % i == 0)
-			return (0);
+		return (0);
 	}
-	return (1);
+	if (n == 2)
+	{
+		return (1);
+	}
+	return (is_divisible(n, 2) == 1);
 }
-
