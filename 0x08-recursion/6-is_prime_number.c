e610ec2098cb743d949b558f7eb2cbfdc80fce9c
Lebogang <lebogangodeveloper@gmail.com>
2023-03-09 11:14:01 +0200

    change return value

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,11 +13,11 @@ int is_divisible(int num, int div)
 {
 	if (num % div == 0)
 	{
-		return (1);
+		return (0);
 	}
 	if (div * div > num)
 	{
-		return (0);
+		return (1);
 	}
 	return (is_divisible(num, div + 1));
 }
