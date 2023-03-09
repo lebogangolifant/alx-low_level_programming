72b08d0fe15865f8c417f94587a0b6b90ac2ffdd
Lebogang <lebogangodeveloper@gmail.com>
2023-03-09 10:15:43 +0200

    add helper func

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,36 +1,40 @@
 #include "main.h"
-#include <math.h>
-
-int power_root(int n, int i);
 
 /**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: input number.
- * Return: natural square root.
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
+ *
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
+ */
+
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
+		return (-1);
+
+	return (find_sqrt(num, root + 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square root of.
+ *
+ * Return: If n has a natural square root - the natural square root of n.
+ *         If n does not have a natural square root - -1.
  */
 
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
+
 	if (n == 0 || n == 1)
 		return (n);
-	return (power_root(n, 1) ? (int)sqrt(n) : -1);
+
+	return (find_sqrt(n, 1));
 }
-
-/**
- * power_root - check natural square root
- * @n: input number
- * @i: counter
- * Return: 1 if has square root, 0 if not
- */
-
-int power_root(int n, int i)
-{
-	if (i * i == n)
-		return (1);
-	if (i * i > n)
-		return (0);
-	return (power_root(n, i + 1));
-}
-
