7a7e03f2f0cc7c234c2c7a7ddd59d443eed9eb44
Lebogang <lebogangodeveloper@gmail.com>
2023-03-11 11:24:37 +0200

    prints num of argumnts passed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of strings containing the arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
+
