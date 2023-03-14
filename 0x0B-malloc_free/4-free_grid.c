3f5284ff1d1ddff8a3eef9ef461eb7020a0886ae
Lebogang <lebogangodeveloper@gmail.com>
2023-03-14 11:39:27 +0200

    func frees a 2D grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * free_grid - Frees a 2D grid previously created by alloc_grid
+ * @grid: The 2D grid to free
+ * @height: The height of the grid
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
+
