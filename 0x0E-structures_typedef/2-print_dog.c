cbbab4429c59ed24aa25c3a008e5772747cc1744
Lebogang <lebogangodeveloper@gmail.com>
2023-03-21 10:43:01 +0200

    Add print_dog function to print struct dog attributes
    The print_dog function takes a struct dog pointer as input and prints the
    name, age, and owner attributes of the struct. If any of these attributes
    are NULL, it prints (nil) instead. This function is useful for debugging
    and testing purposes, and can be called from other parts of the codebase.

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - Prints the attributes of a struct dog
+ * @d: The struct dog to print
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+}
+
