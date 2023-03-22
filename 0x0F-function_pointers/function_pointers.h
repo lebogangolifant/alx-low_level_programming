e1c914816546fe8d02c9e7b65abf0eda4561b40c
Lebogang <lebogangodeveloper@gmail.com>
2023-03-22 12:11:43 +0200

    Add stddef.h header to define NULL pointer constant

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
