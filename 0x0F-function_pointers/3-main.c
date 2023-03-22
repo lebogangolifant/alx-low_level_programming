7e41b586ddf8a03739039232c94e8566fd3881ee
Lebogang <lebogangodeveloper@gmail.com>
2023-03-22 12:35:12 +0200

    Add C program to perform simple operations with betty style compliance
    Implemented a C program that performs basic arithmetic operations (+, -, *, /, %) on two integers passed as arguments to the program. The program uses a structure to define the operations and their corresponding functions, and a function to select the correct function based on the operator passed as an argument. The code follows the betty style guide and includes separate files for each component. Additionally, the program handles errors such as incorrect number of arguments, invalid operators, and division by zero.

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 if successful, otherwise a status code
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
+			&& num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	result = op_func(num1, num2);
+
+	printf("%d\n", result);
+
+	return (0);
+}
