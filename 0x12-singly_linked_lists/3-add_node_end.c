671faf96cf9a617c153da36093bce5a2d5ee69ce
Lebogang <lebogangodeveloper@gmail.com>
2023-03-30 11:36:47 +0200

    Add add_node_end function to append nodes to a list
    This commit adds a new function, `add_node_end`, to the program. The
    function takes a pointer to the head of a linked list and a string to be
    added as the content of a new node. The function appends the new node to
    the end of the list.

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: Pointer to the head of the list.
+ * @str: String to be added as the content of the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *temp_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	for (temp_node = *head; temp_node->next != NULL;
+			temp_node = temp_node->next)
+		;
+
+	temp_node->next = new_node;
+	return (new_node);
+}
+
