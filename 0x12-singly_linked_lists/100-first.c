ab4ed1f364d0fc698c7462db0924a91d1a69fc3f
Lebogang <lebogangodeveloper@gmail.com>
2023-03-30 11:51:33 +0200

    Add startup message and constructor function attribute
    This commit adds a startup message to the program that is printed to the standard output stream before the main function is executed. The constructor function attribute is used to ensure that the message is automatically called before the main function. This feature enhances the user experience by providing a friendly greeting and a fun reference to a famous literary character. No functionality is changed in this commit.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_message - prints a message
+ */
+
+void print_message(void) __attribute__((constructor));
+
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
