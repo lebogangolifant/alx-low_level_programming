e9ad3e4746c7a8ea492f792eb7e2bd17df028fb5
Lebogang <lebogangodeveloper@gmail.com>
2023-03-30 14:02:59 +0200

    Fix printout issue in linked list output

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,35 +7,38 @@
  *
  * Return: The address of the new element, or NULL if it failed.
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *temp_node;
+        list_t *new_node, *temp_node;
+        size_t len = 0;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+        while (str[len])
+                len++;
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+        new_node = malloc(sizeof(list_t));
+        if (new_node == NULL)
+                return (NULL);
 
-	new_node->next = NULL;
+        new_node->str = strdup(str);
+        if (new_node->str == NULL)
+        {
+                free(new_node);
+                return (NULL);
+        }
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
+        new_node->len = len;
+        new_node->next = NULL;
 
-	for (temp_node = *head; temp_node->next != NULL;
-			temp_node = temp_node->next)
-		;
+        if (*head == NULL)
+        {
+                *head = new_node;
+                return (new_node);
+        }
 
-	temp_node->next = new_node;
-	return (new_node);
+        for (temp_node = *head; temp_node->next != NULL; temp_node = temp_node->next)
+                ;
+
+        temp_node->next = new_node;
+
+        return (new_node);
 }
-
