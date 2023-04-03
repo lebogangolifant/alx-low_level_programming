ae70c75b752c507a5019dd0499265b4ccec4056d
Lebogang <lebogangodeveloper@gmail.com>
2023-04-03 12:15:59 +0200

    Implement print_listint function
    This commit adds an implementation of the `print_listint` function to the codebase. The function takes a pointer to the head node of a linked list of `listint_t` elements and prints the integer value of the `n` member of each node to the console, one value per line. The function returns the number of nodes in the linked list as a `size_t`.
    
    The implementation uses a `for` loop to iterate over the linked list, starting at the head node and continuing until the end of the list is reached. The loop body prints the integer value of the `n` member of each node using `printf` and increments a counter variable. The final value of the counter is returned as the function result.

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: Pointer to the head node of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *node;
+
+	for (node = h; node != NULL; node = node->next)
+	{
+		printf("%d\n", node->n);
+		count++;
+	}
+
+	return count;
+}
