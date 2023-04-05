75a3f96f047e54634a97c3e9fe6b9cef907ff605
Lebogang <lebogangodeveloper@gmail.com>
2023-04-05 10:33:16 +0200

    Fix error in print_listint_safe function
    This commit fixes an error in the print_listint_safe function that caused it to return an incorrect count of the number of nodes in the linked list, and to fail to detect loops in the list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,18 +11,25 @@ size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *current;
 	size_t count = 0;
+	unsigned long int visited_nodes[1024] = {0};
+	int index;
 
-	for (current = head; current; current = current->next, count++)
+	for (current = head; current; current = current->next)
 	{
 		printf("[%p] %d\n", (void *) current, current->n);
+		count++;
 
-		if (current <= current->next)
+		index = (unsigned long int) current % 1024;
+		if (visited_nodes[index] != 0)
 		{
 			printf("-> [%p] %d\n", (void *) current->next,
 					current->next->n);
 			exit(98);
 		}
+
+		visited_nodes[index] = (unsigned long int) current;
 	}
+
 	return (count);
 }
 
