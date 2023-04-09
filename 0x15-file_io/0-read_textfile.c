66b7987b29fba34c2ae34779ad77f8d4ea4911ae
Lebogang <lebogangodeveloper@gmail.com>
2023-04-09 15:00:08 +0200

    Fix parameter descriptions in read_textfile function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,7 +3,7 @@
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of the file to read
- * @num_letters: number of letters to read and print
+ * @letters: number of letters to read and print
  *
  * Return: the actual number of letters it could read and print
  */
