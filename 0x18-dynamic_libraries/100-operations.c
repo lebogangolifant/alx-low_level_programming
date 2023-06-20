c8991f178c967243a250cb1698a52ae195a8eed7
Lebogang <lebogangodeveloper@gmail.com>
2023-06-20 10:14:47 +0200

    Fix undefined symbol error in dynamic library

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * int add(int a, int b) - Returns the sum of two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ * Return: The sum of a and b.
+ */
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * int sub(int a, int b) - Returns the difference between two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ * Return: The difference between a and b.
+ */
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * int mul(int a, int b) - Returns the product of two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ * Return: The product of a and b.
+ */
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * int div(int a, int b) - Returns the quotient of two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ * Return: The quotient of a and b.
+ */
+int divide(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * int mod(int a, int b) - Returns the remainder of the division of two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ * Return: The remainder of the division of a by b.
+ */
+int mod(int a, int b)
+{
+    return (a % b);
+}
