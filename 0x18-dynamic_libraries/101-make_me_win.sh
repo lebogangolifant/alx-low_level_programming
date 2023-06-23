f46240ab696829302486b56e6a59ae9fe92e07d4
Lebogang <lebogangodeveloper@gmail.com>
2023-06-23 11:12:36 +0000

    Code injection script

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P .. https://raw.githubusercontent.com/lebogangolifant/alx-low_level_programming/master/0x18-dynamic_libraries/libgm.so
-export LD_PRELOAD="$PWD/../libgm.so
+export LD_PRELOAD="$PWD/../libgm.so"
