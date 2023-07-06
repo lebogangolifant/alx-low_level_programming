b3893f6d90921fcfa5c8e79fcab7c0ea30f2185d
Lebogang <lebogangodeveloper@gmail.com>
2023-07-06 13:21:33 +0200

    Retrieve a value from a hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: The hash table.
+ * @key: The key string.
+ *
+ * Return: The value associated with the element.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	for (node = ht->array[index]; node != NULL; node = node->next)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+	}
+
+	return (NULL);
+}
